d82ddf0706c65611d19df14dc3428a2a1cec1b25
gitsachin <qasachin03@gmail.com>
2023-02-20 19:40:46 +0530

    added client master page file

---
+++ src/main/java/Pages/ClientMasterPage.java
@@ -0,0 +1,68 @@
+package Pages;
+
+import Utils.SelenideTools;
+import base.PageTools;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+
+public class ClientMasterPage extends PageTools {
+
+	By clientMasterPageTitle = By.xpath("//p[@class=' li-text']");
+	By addClientButton = By.xpath("//button[text()='ADD Client ']");
+	By addClientsPopupWindowTitle = By.xpath("//p[@class='company-1 mb-1']");
+	By addClientTableLabels = By.xpath("//*[@id=\"modal-content\"]/div/div[2]//label");
+
+	public boolean isClientMasterPageOpened() {
+		waitForElementVisibility(clientMasterPageTitle);
+		return isElementVisible(clientMasterPageTitle);
+	}
+
+	public void clickSubmitNewJobButton() {
+		waitForElementClickable(addClientButton);
+		click(addClientButton);
+		SelenideTools.sleep(1);
+	}
+
+	public boolean isAddClientPopupWindowOpened() {
+		waitForElementVisibility(addClientsPopupWindowTitle);
+		return isElementVisible(addClientsPopupWindowTitle);
+	}
+
+	public boolean isAddClientTableLabelsPresent(List<String> labels) {
+		List<String> tableLabelsList = new ArrayList<>();
+		List<SelenideElement> elements = getElements(addClientTableLabels);
+		for (SelenideElement element : elements) {
+			tableLabelsList.add(element.getText());
+			System.out.println(element.getText());
+		}
+		System.out.println("-------------------------");
+		System.out.println("Verifying the data tables");
+		System.out.println("-------------------------");
+		tableLabelsList.add(getSelenideElement(addClientTableLabels).getText());
+		for (int i = 0; i < labels.size(); i++) {
+			System.out.println(labels.get(i) + " " + tableLabelsList.get(i));
+
+//			if(tableLabelsList.get(i).isEmpty() == true) {
+//				System.out.println("Blank field name found on UI");
+//			}
+//			else {
+			if ((labels.get(i) == null)) {
+				System.out.println("Value is null");
+			} else if (!labels.get(i).equals(tableLabelsList.get(i))) {
+				System.out.println("Value is not equal");
+//				return false;
+			}
+//				continue;
+		}
+		return true;
+//		}
+		// return tableLabelsList.retainAll(labels);
+//		return false;
+	}
+}
