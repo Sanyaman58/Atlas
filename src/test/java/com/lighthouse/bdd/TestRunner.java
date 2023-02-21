b72208b2662e30ab690f19796b47535b20193120
gitsachin <qasachin03@gmail.com>
2023-02-21 10:50:03 +0530

    updated test runner file

--- src/test/java/com/lighthouse/bdd/TestRunner.java
+++ src/test/java/com/lighthouse/bdd/TestRunner.java
@@ -5,7 +5,6 @@ import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(
-//        tags="@C11",
         features = "src/test/resources/features",
         plugin = {"html:cucumber-reports/html/report.html",
                 "json:cucumber-html-reports/cucumber.json",
