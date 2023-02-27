35e3e6ed53a662064284e24e53a995e0cc3e96bc
gitsachin <qasachin03@gmail.com>
2023-02-27 19:54:27 +0530

    updated step definitions file

--- src/test/java/com/lighthouse/bdd/steps/ManagementAdminPageStepDefinitions.java
+++ src/test/java/com/lighthouse/bdd/steps/ManagementAdminPageStepDefinitions.java
@@ -30,5 +30,46 @@ public class ManagementAdminPageStepDefinitions {
 	public void theFollowingAddClientLabelsAreDisplayed(List<String> listOfLabels) {
         Pages.clientMasterPage().isAddClientTableLabelsPresent(listOfLabels);
     }
+	
+	@And("Check or uncheck the jurisdiction checkbox on [End Up in States]")
+	public void checkOrUncheckJurisdictionCheckbox() {
+        Pages.clientMasterPage().chekcUncheckEuiCheckbox();
+    }
+	
+	@Then("Click on EUI save button")
+    public void clickEuiSaveButton() {
+        Pages.clientMasterPage().jurisdictionEuiSaveButton();
+    }
+	
+	@And("Approve and Activate the newly created EUI version")
+    public void approveAndActivateEuiVersion() {
+        Pages.clientMasterPage().approveActivateEUIVersion();
+    }
+	
+	@And("Verify all the Jurisdictions are displayed in [Jurisidction] dropdown")
+    public void verifyAllJurisdictionOnRequirementAdminPage() {
+        Pages.clientMasterPage().jurisdictionsInJurisdictionDropdown();
+    }
+	
+	@And("Select {int} value from [Requirement Category] dropdown")
+    public void selectValueFronRequirementCategory(int index) {
+        Pages.clientMasterPage().selectFromRequirementCategoryDropdown(index);
+    }
+	
+	@And("Select {int} value from [Requirement Type] dropdown")
+    public void selectValueFronRequirementType(int index) {
+        Pages.clientMasterPage().selectFromRequirementCategoryDropdown(index);
+    }
+	
+	@And("Click on [Create New] button")
+    public void clickCreateNewButton() {
+        Pages.clientMasterPage().createNewButton();
+    }
+	
+	@And("Click on [General information] Jurisdiction dropdown and verify the count")
+    public void verifyAllJurisdictionOnGeneralInfoPage() {
+        Pages.clientMasterPage().jurisdictionsInGeneralInfoPage();
+    }
+	
 
 }
