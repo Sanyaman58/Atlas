35799e2d98018fdebd108c52f4aec3db5a61b5c0
gitsachin <qasachin03@gmail.com>
2023-02-28 16:30:07 +0530

    updated methods on client master page

--- src/main/java/Pages/ClientMasterPage.java
+++ src/main/java/Pages/ClientMasterPage.java
@@ -24,7 +24,7 @@ public class ClientMasterPage extends PageTools {
 	By addClientTableLabels = By.xpath("//*[@id=\"modal-content\"]/div/div[2]//label");
 	By jurisdictionPageFirstCheckbox = By.xpath("//*[@id=\"Alabama\"]");
 	By EuiSaveButton = By.xpath("//*[@id=\"save_state\"]");
-	
+
 	By EuiVersionApproveButton = By.xpath("//button[@class=\"approve approve-document\"]");
 	By EuiApprovePopupYesButton = By.xpath("//button[@id=\"yes_btn\"]");
 	By versionedEuiPageVersionHeader = By.xpath("//*[@id=\"DataTables_Table_0\"]/thead/tr/th[3]/div/label");
@@ -35,13 +35,22 @@ public class ClientMasterPage extends PageTools {
 	By requirementCreateNewButton = By.xpath("//*[@id=\"start_new\"]");
 	By genralInfoPageJurisdictionDropdown = By.xpath("//*[@id=\"Jurisdiction\"]");
 	By stateCheckboxes = By.xpath("//form[@id='mange-job-research']//div[@class='row']//div[@class='list-checkbox']");
-
+	By surveillanceIntelligenceDropdown = By.xpath("//*[@id=\"menu\"]/li[5]/a");
+	By configurationDropdownButton = By.xpath("//*[@id=\"menu\"]/li[5]/ul/li[1]/a");
+	By resultsDropdownButton = By.xpath("//*[@id=\"menu\"]/li[5]/ul/li[2]/a");
+	By activityLogOption = By.xpath("//*[@id=\"DataTables_Table_0\"]/tbody/tr[1]/td[6]/div/a");
+	By activityLogTitle = By.xpath("//*[@id=\"wrapper\"]/main/div/section/div/div[1]/h2");
+	By activityLogFilter = By.xpath("//*[@id=\"DataTables_Table_0_length\"]/label/select");
 	
-	HashMap<String,Boolean> statesCheckboxes = new HashMap<>();
-	HashMap<String,Boolean> statesCheckboxesToCompare = new HashMap<>();
+	
+	
+
+	HashMap<String, Boolean> statesCheckboxes = new HashMap<>();
+	HashMap<String, Boolean> statesCheckboxesToCompare = new HashMap<>();
 	String categoryName;
 	String requirementTypeName;
-	
+	String All;
+
 	public boolean isClientMasterPageOpened() {
 		waitForElementVisibility(clientMasterPageTitle);
 		return isElementVisible(clientMasterPageTitle);
@@ -78,115 +87,148 @@ public class ClientMasterPage extends PageTools {
 			}
 		}
 	}
-	
+
 	public void chekcUncheckEuiCheckbox() {
-		SelenideElement jurisdictionPageFirstCheckboxElement = getSelenideElement(jurisdictionPageFirstCheckbox).shouldBe(Condition.visible);
-		if(jurisdictionPageFirstCheckboxElement.isSelected()) {
+		SelenideElement jurisdictionPageFirstCheckboxElement = getSelenideElement(jurisdictionPageFirstCheckbox)
+				.shouldBe(Condition.visible);
+		if (jurisdictionPageFirstCheckboxElement.isSelected()) {
 			jurisdictionPageFirstCheckboxElement.click();
 			System.out.println("Unchecked the checkbox");
-		}
-		else {
+		} else {
 			jurisdictionPageFirstCheckboxElement.click();
 		}
 	}
-	
+
 	public void jurisdictionEuiSaveButton() {
 		waitForElementClickable(EuiSaveButton);
 		click(EuiSaveButton);
 		SelenideTools.sleep(1);
 	}
-	
+
 	public void approveActivateEUIVersion() {
-		SelenideElement versionedEuiPageVersionHeaderElement = getSelenideElement(versionedEuiPageVersionHeader).shouldBe(Condition.visible);
+		SelenideElement versionedEuiPageVersionHeaderElement = getSelenideElement(versionedEuiPageVersionHeader)
+				.shouldBe(Condition.visible);
 		versionedEuiPageVersionHeaderElement.doubleClick();
-		
+
 		waitForElementClickable(EuiVersionApproveButton);
 		click(EuiVersionApproveButton);
 		SelenideTools.sleep(1);
-		
+
 		waitForElementVisibility(EuiApprovePopupYesButton);
 		click(EuiApprovePopupYesButton);
 		SelenideTools.sleep(5);
-		
-		
+
 		versionedEuiPageVersionHeaderElement.doubleClick();
 		SelenideTools.sleep(5);
-	
-		SelenideElement EuiVersionActivateButtonElement = getSelenideElement(EuiVersionActivateButton).shouldBe(Condition.visible);
+
+		SelenideElement EuiVersionActivateButtonElement = getSelenideElement(EuiVersionActivateButton)
+				.shouldBe(Condition.visible);
 		EuiVersionActivateButtonElement.click();
-		
+
 		waitForElementVisibility(EuiApprovePopupYesButton);
 		click(EuiApprovePopupYesButton);
 		SelenideTools.sleep(5);
 	}
-	
+
 	public void jurisdictionsInJurisdictionDropdown() {
-		SelenideElement jurisdictionDropdownElement = getSelenideElement(jurisdictionDropwdown).shouldBe(Condition.visible);
+		SelenideElement jurisdictionDropdownElement = getSelenideElement(jurisdictionDropwdown)
+				.shouldBe(Condition.visible);
 		int dropdownElementCount = jurisdictionDropdownElement.findAll("option").size();
 		if (dropdownElementCount == 53) {
 			List<String> options = jurisdictionDropdownElement.findAll("options").texts();
-			System.out.println("Total count of elements present in jurisdiction dropdown = "+dropdownElementCount);
-			System.out.println("Text of elements present in jurisdiction dropdown = "+options);
-		}
-		else {
+			System.out.println("Total count of elements present in jurisdiction dropdown = " + dropdownElementCount);
+			System.out.println("Text of elements present in jurisdiction dropdown = " + options);
+		} else {
 			System.out.println("Count mismatch");
 		}
 	}
-	
-	public void selectFromRequirementCategoryDropdown(int index){
+
+	public void selectFromRequirementCategoryDropdown(int index) {
 		waitForElementClickable(requirementCategoryDropdown);
 		getSelenideElement(requirementCategoryDropdown).selectOption(index);
 		categoryName = getSelenideElement(requirementCategoryDropdown).getText();
 	}
-	
-	public void selectFromRequirementCategoryType(int index){
+
+	public void selectFromRequirementCategoryType(int index) {
 		waitForElementClickable(requirementTypeDropdown);
 		getSelenideElement(requirementTypeDropdown).selectOption(index);
 		requirementTypeName = getSelenideElement(requirementTypeDropdown).getText();
 		SelenideTools.sleep(5);
 	}
-	
+
 	public void createNewButton() {
 		waitForElementClickable(requirementCreateNewButton);
 		click(requirementCreateNewButton);
 		SelenideTools.sleep(6);
 	}
-	
+
 	public void jurisdictionsInGeneralInfoPage() {
-		SelenideElement jurisdictionDropdownOnGeneralInfoElement = getSelenideElement(genralInfoPageJurisdictionDropdown).shouldBe(Condition.visible);
+		SelenideElement jurisdictionDropdownOnGeneralInfoElement = getSelenideElement(
+				genralInfoPageJurisdictionDropdown).shouldBe(Condition.visible);
 		int jurisdictionDropdownElementCount = jurisdictionDropdownOnGeneralInfoElement.findAll("option").size();
 		if (jurisdictionDropdownElementCount == 53) {
 			List<String> options = jurisdictionDropdownOnGeneralInfoElement.findAll("options").texts();
-			System.out.println("Total count of elements present in jurisdiction dropdown = "+jurisdictionDropdownElementCount);
-			System.out.println("Text of elements present in jurisdiction dropdown = "+options);
-		}
-		else {
+			System.out.println(
+					"Total count of elements present in jurisdiction dropdown = " + jurisdictionDropdownElementCount);
+			System.out.println("Text of elements present in jurisdiction dropdown = " + options);
+		} else {
 			System.out.println("Count mismatch");
 		}
 	}
-	
+
 	public void reloadBrowser() {
 		WebDriverRunner.getWebDriver().navigate().refresh();
-		SelenideTools.sleep(6);	}
-	
-	public void getStateCheckboxesValuesAndCount(){
-		for(SelenideElement element : getElements(stateCheckboxes)){
-			statesCheckboxes.put(element.findElement(By.xpath("./label")).getText()
-					,element.findElement(By.xpath("./input")).isSelected());
+		SelenideTools.sleep(6);
+	}
+
+	public void getStateCheckboxesValuesAndCount() {
+		for (SelenideElement element : getElements(stateCheckboxes)) {
+			statesCheckboxes.put(element.findElement(By.xpath("./label")).getText(),
+					element.findElement(By.xpath("./input")).isSelected());
 		}
-		
-		
 		SelenideElement jurisdictionStatesElement = getSelenideElement(stateCheckboxes).shouldBe(Condition.visible);
 		int stateCheckboxesElementCount = jurisdictionStatesElement.findAll("div").size();
 		if (stateCheckboxesElementCount == 52) {
-//			List<String> options = stateCheckboxes.findAll("options").texts();
-			System.out.println("Total count of elements present in jurisdiction dropdown = "+stateCheckboxesElementCount);
-		}
-		else {
+			System.out.println(
+					"Total count of elements present in jurisdiction dropdown = " + stateCheckboxesElementCount);
+		} else {
 			System.out.println("Count mismatch");
 		}
-		
-
 	}
 
+	public void clickSurveillanceDropdown() {
+		waitForElementClickable(surveillanceIntelligenceDropdown);
+		click(surveillanceIntelligenceDropdown);
+		SelenideTools.sleep(1);
+		waitForElementVisibility(configurationDropdownButton);
+		waitForElementVisibility(resultsDropdownButton);
+	}
+	
+	public void clickResulsDropdownButton() {
+		waitForElementVisibility(resultsDropdownButton);
+		click(resultsDropdownButton);
+		SelenideTools.sleep(1);
+	}
+	
+	public void clickActivityLogOfLineItem() {
+		waitForElementVisibility(activityLogOption);
+		click(activityLogOption);
+	}
+
+	public void verifyActivityLogScreen() {
+		waitForElementVisibility(activityLogTitle);
+		waitForElementVisibility(activityLogFilter);
+		
+		SelenideElement activityLogFilterElement = getSelenideElement(activityLogFilter).shouldBe(Condition.visible);
+		activityLogFilterElement.getText();
+		if(activityLogFilterElement.getText() == "All") {
+			System.out.println("Filter is already set to 'All'");
+		}
+		else {
+			System.out.println("Filter is not set to 'All'");
+		}
+		
+	}
+
+	
 }
