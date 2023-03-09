ee49ea68724cb4beb5734572caa7d2793edc8ab4
gitsachin <qasachin03@gmail.com>
2023-03-09 13:14:11 +0530

    updated test runner file

--- src/test/java/com/lighthouse/bdd/TestRunner.java
+++ src/test/java/com/lighthouse/bdd/TestRunner.java
@@ -1,19 +1,17 @@
 package com.lighthouse.bdd;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-
-@CucumberOptions(
-        features = "src/test/resources/features",
-        plugin = {"html:cucumber-reports/html/report.html",
-                "json:cucumber-html-reports/cucumber.json",
-                "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
-        )
+@CucumberOptions(features = "src/test/resources/features", plugin = {
+		"html:cucumber-reports/html/report.html", "json:cucumber-html-reports/cucumber.json", "pretty",
+		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
 
 public class TestRunner extends AbstractTestNGCucumberTests {
-        @Override
-        @DataProvider(parallel = false)
-        public Object[][] scenarios() {
-                return super.scenarios();}
+	@Override
+	@DataProvider(parallel = false)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
 }
