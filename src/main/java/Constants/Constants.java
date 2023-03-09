78f38548870258899e2825e94e8e5cf0115dd7a7
gitsachin <qasachin03@gmail.com>
2023-03-09 13:12:36 +0530

    updated constants file

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	public static String REMOTE_URL = System.getProperty("remoteurl");
 
 	public static String URL = "https://d23n8hprwcya6o.cloudfront.net";
-	public static String ATLAS_URL = "https://qaatlas.pharma.solutions/login";
+	public static String ATLAS_URL = "https://qa-atlas.lha.pharma.solutions/";
 	public static String ADMIN_URL = "https://d1q473yljb185n.cloudfront.net/";
 	public static String Username = "nikita.danilevskiy@pharma.solutions";
 	public static String Password = "Testing2022!!";
