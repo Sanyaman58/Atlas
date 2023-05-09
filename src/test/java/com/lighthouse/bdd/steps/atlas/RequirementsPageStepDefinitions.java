a0f8ec09ee370c10f627fbceb4941158fbef3df6
gitsachin <qasachin03@gmail.com>
2023-05-09 14:44:21 +0530

    added step definitions on requirement page step definitions file

--- src/test/java/com/lighthouse/bdd/steps/atlas/RequirementsPageStepDefinitions.java
+++ src/test/java/com/lighthouse/bdd/steps/atlas/RequirementsPageStepDefinitions.java
@@ -72,5 +72,20 @@ public class RequirementsPageStepDefinitions {
         Assert.assertFalse(Pages.requirementsPage().isBusinessModelLabelVisible());
         Assert.assertFalse(Pages.requirementsPage().isChangeNoteLabelVisible());
     }
+    
+    @Then("The [Company Profile] page is opened")
+    public void theCompanyProfilePageIsOpened() {
+        Pages.requirementsPage().isCompanyProfilePageOpened();
+    }
+    
+    @Then("Click on Profile [View Details] button")
+    public void clickViewDetails() {
+        Pages.requirementsPage().clickCompanyViewDetailsButton();
+    }
+    
+    @Then("Verify the navigation between the company profile pages")
+    public void verifyCopanyProfileNavigations() {
+        Pages.requirementsPage().verifyNavigationBetweenCompanyProfilePages();
+    }
 
 }
